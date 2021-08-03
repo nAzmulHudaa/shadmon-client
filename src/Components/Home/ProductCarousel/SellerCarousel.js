b6d4af2280d5a752b315265135b1b52546fa0310
nAzmulHudaa <your_email@example.comasdasasdasdasaadasdsadaasd>
2021-08-03 17:42:44 +0600

    did the design'

--- src/Components/Home/ProductCarousel/SellerCarousel.js
+++ src/Components/Home/ProductCarousel/SellerCarousel.js
@@ -4,61 +4,106 @@ import building from '../../../images/building1.jpg'
 import building2 from '../../../images/building3.jpg';
 import './SellerCarousel.css'
 import { Avatar } from 'primereact/avatar';
-import { AvatarGroup } from 'primereact/avatargroup';
 
 
 const SellerCarousel = () => {
-    const responsiveLayout = [
-        [
-          { breakpoint: 1000, cols: 3 },
-          { breakpoint: 750, cols: 2, rows: 1, gap: 5 },
-          { breakpoint: 499, autoplay: 2000, loop: true }
-        ]
-      ]
-      return (
-        <div className=" seller-carousel rounded  mb-3">
-            <div className="d-flex justify-content-between ">
-                <h6 className='ms-3'>Connect With Sellers</h6>
-                <p className='mb-0 me-3'>See all</p>
-            </div>
-       
-          <Carousel
-            cols={3} rows={1} gap={10} loop responsiveLayout ={responsiveLayout}
-          >
-            <Carousel.Item>
-            <Avatar image={building} className="p-mr-2 seller-carousel-img" shape="circle" />
+  const responsiveLayout = [
+    [
+      { breakpoint: 1000, cols: 3 },
+      { breakpoint: 750, cols: 2, rows: 1, gap: 5 },
+      { breakpoint: 499, autoplay: 2000, loop: true }
+    ]
+  ]
+  return (
+    <div className=" seller-carousel rounded  mb-3">
+      <div className="d-flex justify-content-between ">
+        <h6 className='ms-3'>Connect With Sellers</h6>
+        <h6 className='mb-0 me-3'>See all</h6>
+      </div>
 
-             
-            
-            </Carousel.Item>
-            <Carousel.Item>
-            <Avatar image={building2} className="p-mr-2 seller-carousel-img" shape="circle" />
-            
+      <Carousel
+        cols={3} rows={1} gap={10} loop responsiveLayout={responsiveLayout}
+      >
+        <Carousel.Item>
+          <Avatar image={building} className="p-mr-2 seller-carousel-img" shape="circle" />
+          <div className="seller-details-top">
+            <h6 className='m-0'>12988</h6>
+            <p className='mb-0'>People Connected</p>
+          </div>
+          <div className="seller-details">
+            <h5 className='m-0'>Anik Traders</h5>
+            <p className="text-secondary mb-0">Electric & Electronics <br />Dhaka</p>
+
+            <button>Connect & See</button>
+          </div>
 
 
-            </Carousel.Item>
-            <Carousel.Item>
-           
-            <Avatar image={building} className="p-mr-2 seller-carousel-img" shape="circle" />
-            
-            </Carousel.Item>
-            <Carousel.Item>
-            
-            <Avatar image={building2} className="p-mr-2 seller-carousel-img" shape="circle" />
-             
-            </Carousel.Item>
-            <Carousel.Item>
-            
-            <Avatar image={building} className="p-mr-2 seller-carousel-img" shape="circle" />
-          </Carousel.Item>
-          <Carousel.Item>
-            
+        </Carousel.Item>
+        <Carousel.Item>
           <Avatar image={building2} className="p-mr-2 seller-carousel-img" shape="circle" />
-           
-          </Carousel.Item>
-    
-          </Carousel>
-        </div>
-)};
+          <div className="seller-details-top">
+            <h6 className='m-0'>12988</h6>
+            <p className='mb-0'>People Connected</p>
+          </div>
+          <div className="seller-details">
+            <h5 className='m-0'>Anik Traders</h5>
+            <p className="text-secondary mb-0">Electric & Electronics <br />Dhaka</p>
+            <button>Connect & See</button>
+          </div>
+
+        </Carousel.Item>
+        <Carousel.Item>
+          <Avatar image={building} className="p-mr-2 seller-carousel-img" shape="circle" />
+          <div className="seller-details-top">
+            <h6 className='m-0'>12988</h6>
+            <p className='mb-0'>People Connected</p>
+          </div>
+          <div className="seller-details">
+            <h5  className='m-0'>Anik Traders</h5>
+            <p className="text-secondary mb-0">Electric & Electronics <br />Dhaka</p>
+            <button>Connect & See</button>
+          </div>
+
+        </Carousel.Item>
+        <Carousel.Item>
+          <Avatar image={building2} className="p-mr-2 seller-carousel-img" shape="circle" />
+          <div className="seller-details-top">
+            <h6 className='m-0'>12988</h6>
+            <p className='mb-0'>People Connected</p>
+          </div>
+          <div className="seller-details">
+            <h5  className='m-0'>Anik Traders</h5>
+            <p className="text-secondary mb-0">Electric & Electronics <br />Dhaka</p>
+            <button>Connect & See</button>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Avatar image={building} className="p-mr-2 seller-carousel-img" shape="circle" />
+          <div className="seller-details-top">
+            <h6 className='m-0'>12988</h6>
+            <p className='mb-0'>People Connected</p>
+          </div>
+          <div className="seller-details">
+            <h5  className='m-0'>Anik Traders</h5>
+            <p className="text-secondary mb-0">Electric & Electronics <br />Dhaka</p>
+            <button>Connect & See</button>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Avatar image={building2} className="p-mr-2 seller-carousel-img" shape="circle" />
+          <div className="seller-details-top">
+            <h6 className='m-0'>12988</h6>
+            <p className='mb-0'>People Connected</p>
+          </div>
+          <div className="seller-details">
+            <h5  className='m-0'>Anik Traders</h5>
+            <p className="text-secondary mb-0">Electric & Electronics <br />Dhaka</p>
+            <button>Connect & See</button>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  )
+};
 
 export default SellerCarousel;
