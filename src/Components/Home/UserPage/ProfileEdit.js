0a4d3c6847904f2201d5deaf820394d9a540a5ff
nAzmulHudaa <your_email@example.comasdasasdasdasaadasdsadaasd>
2021-08-12 13:45:40 +0600

    profile page almost done

--- src/Components/Home/UserPage/ProfileEdit.js
+++ src/Components/Home/UserPage/ProfileEdit.js
@@ -3,19 +3,103 @@ import React, { useState } from 'react';
 
 
 const ProfileEdit = () => {
-    const [value1, setValue1] = useState('');
+
 
     return (
         <div className='profile-edit'>
-            <div className="profile-contents">
+            <div className="profile-edit-contents">
                 <h6 className='text-secondary'>BASIC INFORMATION</h6>
                 <div className="basic-inputs">
-                    <input type="text" placeholder="Profile Created By" />
-                </div>
+                    <div className='d-flex'>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Profile Created By</label>
+                        </div>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Location</label>
+                        </div>
+                    </div>
+                    <div className='d-flex'>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Name</label>
+                        </div>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Email</label>
+                        </div>
+                    </div>
+                    <div className='d-flex'>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Mobile Verified</label>
+                        </div>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Add Mobile Number</label>
+                        </div>
+                    </div>
+                    <div className='d-flex'>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Date Of Birth</label>
+                        </div>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Gender</label>
+                        </div>
+                    </div>
+                    <div className='d-flex'>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Education</label>
+                        </div>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Current Job</label>
+                        </div>
+                    </div>
+                    <div className='d-flex'>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Job Experience</label>
+                        </div>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Job Experience</label>
+                        </div>
+                    </div>
+                    <div className='form-full'>
+                        <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                        <label htmlFor="profile" className="form_label">Full Address</label>
+                    </div>
+                    <h6 className='text-secondary mt-5'>Seller INFORMATION</h6>
+                    <div className='d-flex'>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Shop Name</label>
+                        </div>
+                        <div className='form '>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Communicate By</label>
+                        </div>
+                    </div>
+                    <div className='form-full-seller d-flex'>
+                        <div>
+                            <input type="text" id="profile" className="form_input" autoComplete="off" autoCorrect />
+                            <label htmlFor="profile" className="form_label">Full Address</label>
+                        </div>
+                        <div style={{position:'relative',left:'343px'}}>
+                            <button className="seller-btn">Change</button>
+                        </div>
+                        <div style={{position:'relative',left:'345px'}}>
+                            <button className="seller-btn1">Save</button>
+                        </div>
 
-              
+                    </div>
+                </div>
             </div>
-            
         </div>
     );
 };
